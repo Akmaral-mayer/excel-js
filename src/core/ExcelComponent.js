3ea40540c7a566417512f1d2083535634bbf8a2b
Akmaral <mayerwell@MacBook-Pro-Akmaral.local>
2021-01-20 12:15:01 +0600

    modified classes

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,15 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
   // возвращает шаблон компонента
   toHTML() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
   }
 }
