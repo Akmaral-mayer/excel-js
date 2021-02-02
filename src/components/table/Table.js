35a23d9caeaa43d34b87774073501f27a105f94a
Akmaral <mayerwell@MacBook-Pro-Akmaral.local>
2021-02-02 18:14:05 +0600

    finished resizing functionality of table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from '@core/dom'
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,22 +17,9 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      // const $parent = $resizer.$el.parentNode // bad!
-      // const $parent = $resizer.$el.closest('.column') // better but bad
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-
-      document.onmousemove = e => {
-        const delta = e.pageX - coords.right
-        const value = coords.width + delta
-        $parent.$el.style.width = value + 'px'
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-      }
+    if (shouldResize(event)) {
+      // eslint-disable-next-line no-undef
+      resizeHandler(this.$root, event)
     }
   }
 }
