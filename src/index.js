dbac6a9059a7e9d671a3087728fb66bfacb43507
mayerwell <akmaralslmv@gmail.com>
2021-02-07 02:06:00 +0600

    Try gh-pages n build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './scss/index.scss'
-import {Router} from "@core/routes/Router";
-import {DashboardPage} from "@/pages/DashboardPage";
-import {ExcelPage} from "@/pages/ExcelPage";
-
-new Router('#app', {
-  dashboard: DashboardPage,
-  excel: ExcelPage
-})
